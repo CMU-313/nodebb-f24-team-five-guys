40568783ecbbca75b95df877015474394aba1796
LeeMEow23 <1799129732@qq.com>
2024-10-28 22:11:44 -0400

    fix linter issue

--- iroh.config.js
+++ iroh.config.js
@@ -1,8 +1,10 @@
+'use strict';
+
 module.exports = {
-    include: ['src/**/*.js'], // Files to include for analysis
-    exclude: ['node_modules/**', 'tests/**'], // Exclude test files or unnecessary paths
-    events: ['call', 'return'], // Specify events to intercept (e.g., function calls, returns)
-    plugins: {
-      runtimeVisualization: true, // Enable runtime visualization
-    },
-  };
+	include: ['src/**/*.js'], // Files to include for analysis
+	exclude: ['node_modules/**', 'tests/**'], // Exclude test files or unnecessary paths
+	events: ['call', 'return'], // Specify events to intercept (e.g., function calls, returns)
+	plugins: {
+		runtimeVisualization: true, // Enable runtime visualization
+	},
+};
