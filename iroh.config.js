a1350512f093dcad9ef074399eb53f0b411a95d4
LeeMEow23 <1799129732@qq.com>
2024-10-28 22:06:44 -0400

    custmoized iroh

---
+++ iroh.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    include: ['src/**/*.js'], // Files to include for analysis
+    exclude: ['node_modules/**', 'tests/**'], // Exclude test files or unnecessary paths
+    events: ['call', 'return'], // Specify events to intercept (e.g., function calls, returns)
+    plugins: {
+      runtimeVisualization: true, // Enable runtime visualization
+    },
+  };
